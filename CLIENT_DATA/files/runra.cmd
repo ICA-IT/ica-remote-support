1272dffed6da22e6d9a65b59ee68cdaa58cf3892
John Kuras <49847323+ICA-IT@users.noreply.github.com>
2019-09-26 15:15:40 -0700

    changed wording of message

--- CLIENT_DATA/files/runra.cmd
+++ CLIENT_DATA/files/runra.cmd
@@ -9,9 +9,8 @@ echo **                                                                         
 echo **   A window titled "Windows Remote Assistance" will appear on your screen shortly.  **
 echo **   If you are not in verbal contact with the person providing help, do so now.      **
 echo **                                                                                    **
-echo **   If you are planning to receive help from John, the best way to contact him is    **
-echo **   via a Whatsapp call (preferably a video call). If you need to contact him via    **
-echo **   land line, you can get his phone number from Matt or Brenda.                     **
+echo **   If you are planning to receive help from John, you can get his phone number      **
+echo **   from Matt or Brenda.                                                             **
 echo **                                                                                    **
 echo **   Please do not press any keys or click on any buttons until directed to do so.    **
 echo **                                                                                    **
