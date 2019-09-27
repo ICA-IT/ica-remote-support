86fff91112bf5c4f6fe8f937c3c0c2343eeffb72
john kuras <jkuras@islandchristianacademy.com>
2019-09-26 20:23:38 -0700

    fixed problem with share permissions and did some wordsmithing

--- CLIENT_DATA/files/runra.cmd
+++ CLIENT_DATA/files/runra.cmd
@@ -1,8 +1,9 @@
 @echo off
 date /t
 time /t
-del \\server\opsi_workbench\dadsoffer.msrcIncident
-cmd /c start msra /saveasfile \\server\opsi_workbench\dadsoffer.msrcIncident password
+if exist \\server\dropbox (
+if exist \\server\dropbox\dadsoffer.msrcIncident del \\server\dropbox\dadsoffer.msrcIncident
+cmd /c start msra /saveasfile \\server\dropbox\dadsoffer.msrcIncident password
 echo.
 echo ****************************************************************************************
 echo **                                                                                    **
@@ -10,12 +11,27 @@ echo **   A window titled "Windows Remote Assistance" will appear on your screen
 echo **   If you are not in verbal contact with the person providing help, do so now.      **
 echo **                                                                                    **
 echo **   If you are planning to receive help from John, you can get his phone number      **
-echo **   from Matt or Brenda.                                                             **
+echo **   from Brenda or Matt.                                                             **
 echo **                                                                                    **
-echo **   Please do not press any keys or click on any buttons until directed to do so.    **
+echo **   Please do not press any keys or click on any buttons until directed to.          **
 echo **                                                                                    **
 echo ****************************************************************************************
 echo.
+) else (
+echo.
+echo ****************************************************************************************
+echo **                                                                                    **
+echo **   ERROR ::                                                                         **
+echo **   Unable to find and access the ICA teachers' dropbox share                        **
+echo **                                                                                    **
+echo **   Only teachers can access remote assistance. If you are a teacher, then this      **
+echo **   is a programming error.                                                          **
+echo **                                                                                    **
+echo **   Please report programming errors to John, Brenda, or Matt.                       **
+echo **                                                                                    **
+echo ****************************************************************************************
+echo.
+)
 pause
 @echo on
 
